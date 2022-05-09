dbd64c3392e8c8c96f3f42ed79bc5e3861631b10
Sadia <sadiawaqas106@gmail.com>
2022-05-09 23:31:51 +0500

    end of section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'The Dating App';
+  users: any;
+
+
+  constructor(private http:HttpClient){}
+
+
+
+
+ngOnInit() {
+    this.getUsers();
+
+  }
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(response =>{
+      this.users = response;
+
+    }, error => {
+      console.log(error);
+    });
+  }
+
+
 }
